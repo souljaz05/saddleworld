acb4ed99f3c299a17269add87ec19458a5e6bc31
souljaz05 <madrigalseiji@gmail.com>
2019-09-18 00:55:52 +1000

    Added font, webpack plugins

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import './styles/scss/main.scss';
+import logosource from './images/logo.png';
+
+var logo = document.getElementById('logo');
+logo.src = logosource;
--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,17 @@ module.exports = {
                     'style-loader',                  
                     'css-loader',
                     'sass-loader']
+            },       {
+                test: /\.(png|jp(e*)g|svg)$/,  
+                use: [{
+                    loader: 'url-loader',
+                    options: { 
+                        limit: 8000, // Convert images < 8kb to base64 strings
+                        name: 'images/[hash]-[name].[ext]'
+                    } 
+                }]
             }
+
         ]
     },
     plugins:[
