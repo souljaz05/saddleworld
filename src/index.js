a05758efc6b352a793af93c234791377a7c3ccb0
souljaz05 <madrigalseiji@gmail.com>
2019-09-18 15:26:55 +1000

    Header new elements, nav bar started

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import './styles/scss/main.scss';
-import logosource from './images/logo.png';
+import logosrc from './images/logo.png';
+import searchlogosrc from './images/search.png';
+import locationsrc from './images/location.png';
+import myaccountsrc from './images/myaccount.png';
+import cartsrc from './images/cart.png';
 
-var logo = document.getElementById('logo');
-logo.src = logosource;
+var logoimg = document.getElementById('logo');
+var searchlogoimg = document.getElementById('search-logo');
+var locationimg = document.getElementById('location');
+var myaccountimg = document.getElementById('my-account');
+var cartimg = document.getElementById('cart');
+
+logoimg.src = logosrc;
+searchlogoimg.src = searchlogosrc;
+locationimg.src = locationsrc;
+myaccountimg.src = myaccountsrc;
+cartimg.src = cartsrc;
