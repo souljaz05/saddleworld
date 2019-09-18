56e3288ad0fff9872275f7f979e9c18058c03594
Seiji M <madrigalseiji@gmail.com>
2019-09-18 19:56:16 +1000

    Added Logos

--- src/index.js
+++ src/index.js
@@ -5,14 +5,40 @@ import locationsrc from './images/location.png';
 import myaccountsrc from './images/myaccount.png';
 import cartsrc from './images/cart.png';
 
+// NAVIGATION BAR
+import rugsrc from './images/rug.png'
+import ridergearsrc from './images/ridergear.png'
+import horsegearsrc from './images/horsegear.png'
+import stablesrc from './images/stable.png'
+import farriersrc from './images/farrier.png'
+import petsrc from './images/pet.png'
+import saddlefitsrc from './images/saddlefit.png'
+
+
 var logoimg = document.getElementById('logo');
 var searchlogoimg = document.getElementById('search-logo');
 var locationimg = document.getElementById('location');
 var myaccountimg = document.getElementById('my-account');
 var cartimg = document.getElementById('cart');
 
+var rugimg = document.getElementById('rug');
+var ridergearimg = document.getElementById('ridergear');
+var horsegearimg = document.getElementById('horsegear');
+var stableimg = document.getElementById('stable');
+var farrierimg = document.getElementById('farrier');
+var petimg = document.getElementById('pet');
+var saddleimg = document.getElementById('saddle');
+
 logoimg.src = logosrc;
 searchlogoimg.src = searchlogosrc;
 locationimg.src = locationsrc;
 myaccountimg.src = myaccountsrc;
 cartimg.src = cartsrc;
+
+rugimg.src = rugsrc;
+ridergearimg.src = ridergearsrc;
+horsegearimg.src = horsegearsrc;
+stableimg.src = stablesrc;
+farrierimg.src = farriersrc;
+petimg.src = petsrc;
+saddleimg.src = saddlefitsrc;
